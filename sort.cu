51d1c348e1062d4cb2467e7a1afae1009867a598
Duong Bao <baosws@gmail.com>
2019-12-25 22:12:10 +0900

    gpu radis sort with k = 1

--- sort.cu
+++ sort.cu
@@ -17,10 +17,10 @@ __global__ void computeHistKernel2(int * in, int n, int * hist, int nBins, int b
         atomicAdd(&hist[idx], s_hist[idx]);
 }
 
-__global__ void scanBlkKernel(int * in, int n, int * out, int * blkSums) {   
-    extern __shared__ int s_in[];
+__global__ void scanBlkKernel(uint32_t * in, int n, uint32_t * out, uint32_t * blkSums, int bit) {   
+    extern __shared__ uint32_t s_in[];
     int id_in = blockDim.x * blockIdx.x + threadIdx.x;
-    s_in[threadIdx.x] = id_in < n ? in[id_in] : 0;
+    s_in[threadIdx.x] = id_in < n ? (bit < 0 ? in[id_in] : ((in[id_in] >> bit) & 1)) : 0;
     __syncthreads();
 
     int turn = 0;
@@ -41,30 +41,31 @@ __global__ void scanBlkKernel(int * in, int n, int * out, int * blkSums) {
         out[id_in] = s_in[threadIdx.x + turn * blockDim.x];
 }
 
-__global__ void sumPrefixBlkKernel(int * out, int n, int * blkSums) {
+__global__ void sumPrefixBlkKernel(uint32_t * out, int n, uint32_t * blkSums) {
     int id_in = blockDim.x * blockIdx.x + threadIdx.x;
     if (id_in < n && blockIdx.x > 0) {
         out[id_in] += blkSums[blockIdx.x - 1];
     }
 }
 
-__global__ void reduceKernel(int * in, int n, int * out) {
+__global__ void reduceKernel(uint32_t * in, int n, uint32_t * out, int bit) {
     int id_in = blockDim.x * blockIdx.x + threadIdx.x;
     if (id_in < n)
-        out[id_in] -= in[id_in];
+        out[id_in] -= ((in[id_in] >> bit) & 1);
 }
 
-void computeScanArray(int* d_in, int* d_out, int n, dim3 blkSize) {
+void computeScanArray(uint32_t* d_in, uint32_t* d_out, int n, dim3 blkSize, int bit) {
     dim3 gridSize((n - 1) / blkSize.x + 1);
 
-    int * d_blkSums;
+    uint32_t * d_blkSums;
     CHECK(cudaMalloc(&d_blkSums, gridSize.x * sizeof(int)));
-    int * d_sum_blkSums;
+    uint32_t * d_sum_blkSums;
     CHECK(cudaMalloc(&d_sum_blkSums, gridSize.x * sizeof(int)));
 
-    scanBlkKernel<<<gridSize, blkSize, 2 * blkSize.x * sizeof(int)>>>(d_in, n, d_out, d_blkSums);
+    scanBlkKernel<<<gridSize, blkSize, 2 * blkSize.x * sizeof(int)>>>
+        (d_in, n, d_out, d_blkSums, bit);
     if (gridSize.x != 1) {
-        computeScanArray(d_blkSums, d_sum_blkSums, gridSize.x, blkSize);
+        computeScanArray(d_blkSums, d_sum_blkSums, gridSize.x, blkSize, -1);
     }
     sumPrefixBlkKernel<<<gridSize, blkSize>>>(d_out, n, d_sum_blkSums);
 
@@ -72,55 +73,56 @@ void computeScanArray(int* d_in, int* d_out, int n, dim3 blkSize) {
     CHECK(cudaFree(d_blkSums));
 }
 
+__global__ void scatterKernel(uint32_t* src, int n, uint32_t* histScan, uint32_t* dst, int bit) {
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    if (i < n) {
+        if ((src[i] >> bit) & 1) {
+            int n0 = n - histScan[n - 1] - ((src[n - 1] >> bit) & 1);
+            dst[n0 + histScan[i]] = src[i];
+        }
+        else
+            dst[i - histScan[i]] = src[i];
+    }
+}
 void sort(const uint32_t * in, int n, uint32_t * out, int k, int * blockSizes) {
-    int nBins = 1 << k; // 2^k
-    int * hist = (int *) malloc(nBins * sizeof(int));
-    int * histScan = (int *) malloc(nBins * sizeof(int));
+    uint32_t * histScan = (uint32_t *) malloc(n * sizeof(int));
 
     uint32_t * src = (uint32_t *)malloc(n * sizeof(uint32_t));
     memcpy(src, in, n * sizeof(uint32_t));
     uint32_t * originalSrc = src; // Use originalSrc to free memory later
-    uint32_t * dst = out;
 
-    int * d_src, * d_hist, * d_histScan;
+    uint32_t * d_src;
+    uint32_t * d_dst;
+    uint32_t * d_histScan;
     CHECK(cudaMalloc(&d_src, n * sizeof(uint32_t)));
-    CHECK(cudaMalloc(&d_hist, nBins * sizeof(int)));
-    CHECK(cudaMalloc(&d_histScan, nBins * sizeof(int)));
+    CHECK(cudaMemcpy(d_src, src, n * sizeof(uint32_t), cudaMemcpyHostToDevice));
+    CHECK(cudaMalloc(&d_dst, n * sizeof(uint32_t)));
+    CHECK(cudaMalloc(&d_histScan, n * sizeof(int)));
 
     // Compute block and grid size for histogram phase
     dim3 blockSizeHist(blockSizes[0]);
     dim3 gridSizeHist((n - 1) / blockSizes[0] + 1);
     dim3 blockSizeScan(blockSizes[1]);
     dim3 gridSizeScan((n - 1) / blockSizes[1] + 1);
-    size_t smemSizeHist = nBins * sizeof(int);
+    dim3 blockSizeScatter = blockSizeScan;
+    dim3 gridSizeScatter((n - 1) / blockSizeScatter.x + 1);
 
-    for (int bit = 0; bit < sizeof(uint32_t) * 8; bit += k) {
-        CHECK(cudaMemcpy(d_src, src, n * sizeof(int), cudaMemcpyHostToDevice));
-        CHECK(cudaMemset(d_hist, 0, nBins * sizeof(uint32_t)));
-
-        computeHistKernel2<<<gridSizeHist, blockSizeHist, smemSizeHist>>>
-            (d_src, n, d_hist, nBins, bit);
-        computeScanArray(d_hist, d_histScan, nBins, blockSizeScan);
-        reduceKernel<<<gridSizeScan, blockSizeScan>>>(d_hist, nBins, d_histScan);
-        CHECK(cudaMemcpy(histScan, d_histScan, nBins * sizeof(int), cudaMemcpyDeviceToHost));
-
-        for (int i = 0; i < n; ++i) {
-            int bin = (src[i] >> bit) & (nBins - 1);
-            dst[histScan[bin]] = src[i];
-            ++histScan[bin];
-        }
-
-        uint32_t * tmp = src;
-        src = dst;
-        dst = tmp;
+    for (int bit = 0; bit < sizeof(uint32_t) * 8; ++bit) {
+        computeScanArray(d_src, d_histScan, n, blockSizeScan, bit);
+        reduceKernel<<<gridSizeScan, blockSizeScan>>>(d_src, n, d_histScan, bit);
+        scatterKernel<<<gridSizeScatter, blockSizeScatter>>>(d_src, n, d_histScan, d_dst, bit);
+        CHECK(cudaDeviceSynchronize());
+        
+        uint32_t * tmp = d_src;
+        d_src = d_dst;
+        d_dst = tmp;
     }
 
-    memcpy(out, src, n * sizeof(uint32_t));
+    CHECK(cudaMemcpy(out, d_src, n * sizeof(uint32_t), cudaMemcpyDeviceToHost));
 
     free(histScan);
     free(originalSrc);
-
+    
     CHECK(cudaFree(d_src));
-    CHECK(cudaFree(d_hist));
     CHECK(cudaFree(d_histScan));
 }
